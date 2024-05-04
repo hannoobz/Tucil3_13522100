c5d3d4613de6762eaa55e0fadac3da74d01e98c0
hannoobz <13522100@std.stei.itb.ac.id>
2024-05-04 14:32:40 +0700

    temporary runner

--- MainRunner.java
+++ MainRunner.java
@@ -1,15 +1,26 @@
+import java.time.Duration;
+import java.time.Instant;
+
+import Dictionary.GameDictionary;
+import Solver.GreedyBFS;
+import WordNode.WordNode;
+
 public class MainRunner {
     public static void main(String[] args) {
         GameDictionary gDictionary = new GameDictionary();
         gDictionary.read("words.txt");
-        GBFS gbfs = new GBFS("kill","feel");
+        GreedyBFS gbfs = new GreedyBFS("love","kiss");
+        Instant startTime = Instant.now();
         gbfs.solve(gDictionary);
+        Instant endTime = Instant.now();
 
         WordNode currentNode = gbfs.getTargetNode();
         while (currentNode!=null) {
-            currentNode.printNode();
+            System.out.println("Final:"+currentNode.getWord());
+            // System.out.println(currentNode.getParent());
             currentNode = currentNode.getParent();
         }
+        System.out.println("Runtime: "+ Duration.between(startTime, endTime));
 
     }
 }
